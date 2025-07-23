895aa4e2fe8d55e541ed3f777691fd1e4738c470
leoBello <leobello.wd@gmail.com>
2025-07-23 03:30:46 +0200

    add react-device-detect

--- src/components/HomePage/HomePage.tsx
+++ src/components/HomePage/HomePage.tsx
@@ -3,20 +3,23 @@ import './HomePage.scss';
 import AnimatedLetters from '../AnimatedLetters/AnimatedLetters';
 import WaveGrid from '../WaveGrid/WaveGrid';
 import Slider from '../Silder/Slider';
+import { isDesktop } from 'react-device-detect';
 
 const HomePage = () => {
   const [gravity, setGravity] = useState(-25);
   return (
     <div className='home-page-container'>
-      <Slider
-        value={gravity}
-        min={-50}
-        max={50}
-        onChange={(value) => setGravity(value)}
-        label='Gravité'
-        className='gravity-slider'
-        withBackground
-      />
+      {isDesktop && (
+        <Slider
+          value={gravity}
+          min={-50}
+          max={50}
+          onChange={(value) => setGravity(value)}
+          label='Gravité'
+          className='gravity-slider'
+          withBackground
+        />
+      )}
       <AnimatedLetters
         className='hero-title no-break-words'
         text='This website is coming soon'
