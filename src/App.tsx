ad633d10d50a72852e1bb4e3431f7a4ca3292d19
leoBello <leobello.wd@gmail.com>
2025-07-24 14:54:06 +0200

    add Toaster

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,24 @@
 import './App.css';
 import HomePage from './components/HomePage/HomePage';
+import { Toaster } from 'react-hot-toast';
 
 function App() {
   return (
     <>
+      <Toaster
+        toastOptions={{
+          style: {
+            background: '#1a1a1a',
+            color: '#fff',
+            boxShadow: '0 4px 12px rgba(0,0,0,0.5)',
+          },
+          iconTheme: {
+            primary: '#0bfffe',
+            secondary: '#dd00d3',
+          },
+        }}
+        position='top-center'
+      />
       <HomePage />
     </>
   );
