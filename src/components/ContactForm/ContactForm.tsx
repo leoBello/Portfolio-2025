c8ccc29a49b9106749b305a9e70b591de799e824
leoBello <leobello.wd@gmail.com>
2025-07-24 14:09:22 +0200

    improve form

--- src/components/ContactForm/ContactForm.tsx
+++ src/components/ContactForm/ContactForm.tsx
@@ -1,4 +1,7 @@
+import React, { useRef } from 'react';
 import { useForm } from 'react-hook-form';
+import gsap from 'gsap';
+import { toast } from 'react-hot-toast'; // <-- Import toast
 import './ContactForm.scss';
 import GenericButton from '../GenericButton/GenericButton';
 
@@ -9,7 +12,9 @@ interface ContactFormInputs {
   message: string;
 }
 
-const ContactForm = () => {
+const EMAIL_PATTERN = /^[\w-.]+@([\w-]+\.)+[\w-]{2,}$/;
+
+const ContactForm: React.FC = () => {
   const {
     register,
     handleSubmit,
@@ -17,38 +22,79 @@ const ContactForm = () => {
     formState: { errors, isSubmitSuccessful },
   } = useForm<ContactFormInputs>();
 
+  const formRef = useRef<HTMLDivElement>(null);
+
+  const triggerGlitch = () => {
+    if (!formRef.current) return;
+
+    const tl = gsap.timeline();
+    tl.to(formRef.current, { x: 10, duration: 0.05 })
+      .to(formRef.current, { x: -10, duration: 0.05 })
+      .to(formRef.current, { x: 0, duration: 0.04 })
+      .to(
+        formRef.current,
+        {
+          filter: 'hue-rotate(80deg) brightness(2) contrast(2)',
+          duration: 0.06,
+        },
+        '-=0.03'
+      )
+      .to(formRef.current, { filter: 'none', duration: 0.06 });
+  };
+
   const onSubmit = (data: ContactFormInputs) => {
+    triggerGlitch();
     console.log('Formulaire soumis :', data);
-    // Ici tu peux intégrer l'envoi vers un backend ou email
+    toast.success('Message envoyé !');
     reset();
   };
 
+  // Affiche les erreurs du formulaire dans un toast à la soumission
+  const onError = (formErrors: typeof errors) => {
+    Object.values(formErrors).forEach((err: any) => {
+      if (err?.message) toast.error(err.message);
+    });
+  };
+
   return (
-    <div className='contact-form-container '>
-      <form className='form' onSubmit={handleSubmit(onSubmit)}>
+    <div ref={formRef} className='contact-form-container'>
+      <form className='form' onSubmit={handleSubmit(onSubmit, onError)}>
         <div className='form-group'>
           <label htmlFor='name'>Name</label>
           <input
+            id='name'
             type='text'
             className='form-input'
             {...register('name', { required: 'Nom obligatoire' })}
             placeholder='Name'
           />
-          {errors.name && (
-            <span className='error'>{`errors?.nom?.message`}</span>
-          )}
         </div>
         <div className='form-group'>
-          <label htmlFor='mail'>Email</label>
+          <label htmlFor='email'>Email</label>
           <input
+            id='email'
             type='email'
-            {...register('email', { required: 'email obligatoire' })}
+            className='form-input'
+            {...register('email', {
+              required: 'Email obligatoire',
+              pattern: {
+                value: EMAIL_PATTERN,
+                message: "Format de l'email incorrect",
+              },
+            })}
             placeholder='Email'
           />
-          {errors.email && (
-            <span className='error'>{`errors.prenom.message`}</span>
-          )}
         </div>
+        {/* <div className='form-group'>
+          <label htmlFor='subject'>Subject</label>
+          <input
+            id='subject'
+            type='text'
+            className='form-input'
+            {...register('subject', { required: 'Sujet obligatoire' })}
+            placeholder='Subject'
+          />
+        </div> */}
         <div className='form-group'>
           <label>Message</label>
           <textarea
@@ -57,17 +103,17 @@ const ContactForm = () => {
             placeholder='Votre message'
             rows={5}
           />
-          {errors.message && (
-            <span className='error'>{`errors.message.message`}</span>
-          )}
         </div>
         <GenericButton
-          handleClick={() => console.log()}
+          handleClick={() => {}}
           className='submit-button'
           type='submit'
           label='Send'
         />
-        {isSubmitSuccessful && <div className='success'>Message envoyé !</div>}
+        {/* Plus besoin d'afficher les .error sous les inputs car c'est géré par toast */}
+        {isSubmitSuccessful && (
+          <div className='success'>Message envoyé&nbsp;!</div>
+        )}
       </form>
     </div>
   );
