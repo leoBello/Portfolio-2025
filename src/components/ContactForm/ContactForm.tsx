ade046199775f31ae6c71cf10ac6f13ed275d21c
leoBello <leobello.wd@gmail.com>
2025-07-24 15:02:20 +0200

    form front-end OK success and error

--- src/components/ContactForm/ContactForm.tsx
+++ src/components/ContactForm/ContactForm.tsx
@@ -1,7 +1,6 @@
 import React, { useRef } from 'react';
-import { useForm } from 'react-hook-form';
-import gsap from 'gsap';
-import { toast } from 'react-hot-toast'; // <-- Import toast
+import { useForm, type FieldErrors } from 'react-hook-form';
+import { toast } from 'react-hot-toast';
 import './ContactForm.scss';
 import GenericButton from '../GenericButton/GenericButton';
 
@@ -15,43 +14,20 @@ interface ContactFormInputs {
 const EMAIL_PATTERN = /^[\w-.]+@([\w-]+\.)+[\w-]{2,}$/;
 
 const ContactForm: React.FC = () => {
-  const {
-    register,
-    handleSubmit,
-    reset,
-    formState: { errors, isSubmitSuccessful },
-  } = useForm<ContactFormInputs>();
+  const { register, handleSubmit, reset } = useForm<ContactFormInputs>();
 
   const formRef = useRef<HTMLDivElement>(null);
 
-  const triggerGlitch = () => {
-    if (!formRef.current) return;
-
-    const tl = gsap.timeline();
-    tl.to(formRef.current, { x: 10, duration: 0.05 })
-      .to(formRef.current, { x: -10, duration: 0.05 })
-      .to(formRef.current, { x: 0, duration: 0.04 })
-      .to(
-        formRef.current,
-        {
-          filter: 'hue-rotate(80deg) brightness(2) contrast(2)',
-          duration: 0.06,
-        },
-        '-=0.03'
-      )
-      .to(formRef.current, { filter: 'none', duration: 0.06 });
-  };
-
   const onSubmit = (data: ContactFormInputs) => {
-    triggerGlitch();
     console.log('Formulaire soumis :', data);
     toast.success('Message envoyé !');
     reset();
   };
 
   // Affiche les erreurs du formulaire dans un toast à la soumission
-  const onError = (formErrors: typeof errors) => {
-    Object.values(formErrors).forEach((err: any) => {
+  const onError = (formErrors: FieldErrors<ContactFormInputs>) => {
+    Object.values(formErrors).forEach((err) => {
+      // err: FieldError | undefined
       if (err?.message) toast.error(err.message);
     });
   };
@@ -89,16 +65,7 @@ const ContactForm: React.FC = () => {
             />
           </div>
         </div>
-        {/* <div className='form-group'>
-          <label htmlFor='subject'>Subject</label>
-          <input
-            id='subject'
-            type='text'
-            className='form-input'
-            {...register('subject', { required: 'Sujet obligatoire' })}
-            placeholder='Subject'
-          />
-        </div> */}
+
         <div className='form-group'>
           <label>Message</label>
           <div className='input-gradient-border'>
@@ -116,10 +83,6 @@ const ContactForm: React.FC = () => {
           type='submit'
           label='Send'
         />
-        {/* Plus besoin d'afficher les .error sous les inputs car c'est géré par toast */}
-        {isSubmitSuccessful && (
-          <div className='success'>Message envoyé&nbsp;!</div>
-        )}
       </form>
     </div>
   );
